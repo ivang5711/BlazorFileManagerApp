76b18d7a2caaa1c187ba5b2b5e2ee01f85a8f4b7
Ivan G <girs874@gmail.com>
2024-02-15 22:22:58 +0400

    Adds FileManager service. Adds Humanizer library.

---
+++ BlazorFileManager/Services/FileManager.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace BlazorFileManager.Services
+{
+    public class FileManager
+    {
+        public List<DriveInfo> GetAllDrives()
+        {
+            return DriveInfo.GetDrives().ToList();
+        }
+    }
+}
