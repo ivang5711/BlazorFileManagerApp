bc08ccb545329d1e4a1915d810e0e217d37343a5
Ivan G <girs874@gmail.com>
2024-02-16 03:00:33 +0400

    Adds folder navigation feature

--- BlazorFileManager/Pages/Index.razor.cs
+++ BlazorFileManager/Pages/Index.razor.cs
@@ -1,28 +1,19 @@
 using BlazorFileManager.Data;
 using BlazorFileManager.Models;
-using Humanizer;
-using Radzen.Blazor;
 using Radzen;
+using Radzen.Blazor;
 
 namespace BlazorFileManager.Pages;
+
 public partial class Index
 {
-    private List<FileSystemDisplayItem> _items;
-    private WeatherForecast[]? forecasts;
-    //private string _driveSelected = string.Empty;
-    //private string _selectedDriveSize = string.Empty;
-    //private List<string> dirs = new();
-    //private List<string> files = new();
-    //private List<System.IO.FileInfo> fileDataList = new();
-    //private DirectoryInfo? _directoryInfo;
+    private List<FileSystemDisplayItem>? _items;
 
     protected override async Task OnInitializedAsync()
     {
-
-        forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
         var drives = _fileManager.GetAllDrives().ToList();
         _items = new();
-        foreach(var drive in drives)
+        foreach (var drive in drives)
         {
             _items.Add(new FileSystemDisplayItem()
             {
@@ -30,67 +21,39 @@ public partial class Index
                 FullName = drive.Name,
                 Parent = string.Empty,
                 Extension = string.Empty,
-                Size = drive.TotalSize,                
+                Size = drive.TotalSize,
             });
         }
-
-        //_driveSelected = drives.First().Name;
-        //_selectedDriveSize = drives.First().TotalSize.Bytes().Humanize();
-
-
-        //_directoryInfo = _fileManager.GetDirectoryInfo(Directory.GetDirectoryRoot(_driveSelected));
-
-        ////fileDataList = _directoryInfo.GetFiles().ToList();
-
-        //foreach(var file in _directoryInfo.GetFiles())
-        //{
-        //    files.Add(file.Name);
-        //}
-
-        //foreach(var directory in _directoryInfo.GetDirectories())
-        //{
-        //    dirs.Add(directory.Name);
-        //}
-
-        ////files.Add(_directoryInfo.GetFiles().First().Name);
-        ////dirs.Add(_directoryInfo.GetDirectories().First().Name);
-        ///
-
-
     }
 
-    string type = "Click";
-    bool multiple = true;
-    //IEnumerable<FileSystemDisplayItem> employees;
-    IList<Tuple<FileSystemDisplayItem, RadzenDataGridColumn<FileSystemDisplayItem>>> selectedCellData = new List<Tuple<FileSystemDisplayItem, RadzenDataGridColumn<FileSystemDisplayItem>>>();
-    //EventConsole console;
+    private readonly string type = "Click";
+    private readonly bool multiple = true;
+    private readonly IList<Tuple<FileSystemDisplayItem, 
+        RadzenDataGridColumn<FileSystemDisplayItem>>> selectedCellData = 
+            new List<Tuple<FileSystemDisplayItem, 
+                RadzenDataGridColumn<FileSystemDisplayItem>>>();
 
-    //protected override async Task OnInitializedAsync()
-    //{
-    //    await base.OnInitializedAsync();
-
-    //    employees = dbContext.Employees;
-    //}
-
-    void Select(DataGridCellMouseEventArgs<FileSystemDisplayItem> args)
+    private void Select(DataGridCellMouseEventArgs<FileSystemDisplayItem> args)
     {
         if (!multiple)
         {
             selectedCellData.Clear();
         }
 
-        var cellData = selectedCellData.FirstOrDefault(i => i.Item1 == args.Data && i.Item2 == args.Column);
+        var cellData = selectedCellData
+            .FirstOrDefault(i => i.Item1 == args.Data && i.Item2 == args.Column);
         if (cellData != null)
         {
             selectedCellData.Remove(cellData);
         }
         else
         {
-            selectedCellData.Add(new Tuple<FileSystemDisplayItem, RadzenDataGridColumn<FileSystemDisplayItem>>(args.Data, args.Column));
+            selectedCellData.Add(new Tuple<FileSystemDisplayItem, 
+                RadzenDataGridColumn<FileSystemDisplayItem>>(args.Data, args.Column));
         }
     }
 
-    void OnCellClick(DataGridCellMouseEventArgs<FileSystemDisplayItem> args)
+    private void OnCellClick(DataGridCellMouseEventArgs<FileSystemDisplayItem> args)
     {
         if (type == "Click")
         {
@@ -98,35 +61,46 @@ public partial class Index
         }
     }
 
-    void OnCellDoubleClick(DataGridCellMouseEventArgs<FileSystemDisplayItem> args)
+    private void OnCellDoubleClick(DataGridCellMouseEventArgs<FileSystemDisplayItem> args)
     {
         if (type != "Click")
         {
             Select(args);
         }
 
-
-        Console.WriteLine("I just double clicked!");
-
-        var g = args.Data.FullName;
-
-        GoToSubFolder(g);
-        StateHasChanged();
-    }
-
-    void OnCellRender(DataGridCellRenderEventArgs<FileSystemDisplayItem> args)
-    {
-        if (selectedCellData.Any(i => i.Item1 == args.Data && i.Item2 == args.Column))
-        {
-            args.Attributes.Add("style", $"background-color: var(--rz-secondary-lighter);");
-        }
+        var path = args.Data.FullName;
+        GoToSubFolder(path);
     }
 
     private void GoToSubFolder(string path)
     {
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            _items = new();
+            AddAllDrivesToDisplayModel();
+            return;
+        }
+
         var dirInfo = _fileManager.GetDirectoryInfo(path);
-        _items = new();
         var folders = dirInfo.GetDirectories();
+        var temp = dirInfo.Parent?.FullName;
+        if (_items[0].FullName == path)
+        {
+            _items = new();
+            temp ??= string.Empty;
+            _items.Add(CreateParentItem(temp));
+            AddAllFoldersToDisplayModel(folders);
+            return;
+        }
+
+        _items = new();
+        temp ??= dirInfo.Root.FullName;
+        _items.Add(CreateParentItem(temp));
+        AddAllFoldersToDisplayModel(folders);
+    }
+
+    private void AddAllFoldersToDisplayModel(DirectoryInfo[] folders)
+    {
         foreach (var folder in folders)
         {
             _items.Add(new FileSystemDisplayItem()
@@ -139,5 +113,33 @@ public partial class Index
                 ModifiedDate = folder.LastWriteTime
             });
         }
+    }
+
+    private void AddAllDrivesToDisplayModel()
+    {
+        var drives = _fileManager.GetAllDrives().ToList();
+        foreach (var drive in drives)
+        {
+            _items.Add(new FileSystemDisplayItem()
+            {
+                Name = drive.Name,
+                FullName = drive.Name,
+                Parent = string.Empty,
+                Extension = string.Empty,
+                Size = drive.TotalSize,
+            });
+        }
+    }
+
+    private FileSystemDisplayItem CreateParentItem(string parent)
+    {
+        return new FileSystemDisplayItem()
+        {
+            Name = "..",
+            FullName = parent,
+            Parent = parent,
+            Extension = string.Empty,
+            Size = 0,
+        };
     }
 }
