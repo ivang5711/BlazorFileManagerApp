f4a031ef56f77b08a3b24b6cf3428c43d7decb74
Ivan G <girs874@gmail.com>
2024-02-17 23:33:11 +0400

    Adds File manager client

--- BlazorFileManager/Pages/Index.razor.cs
+++ BlazorFileManager/Pages/Index.razor.cs
@@ -1,6 +1,6 @@
 using BlazorFileManager.Models;
+using BlazorFileManager.Services;
 using FileManagerDomain.Exceptions;
-using FileManagerDomain.Models;
 using Microsoft.JSInterop;
 using Radzen;
 using Radzen.Blazor;
@@ -11,364 +11,213 @@ namespace BlazorFileManager.Pages;
 public partial class Index
 {
     public string? NewDirectoryName { get; set; }
-    private CurrentFolderViewModel? _currentFolder = new();
-    private readonly string type = "Click";
-    private bool isFolderSelected = false;
+    private RadzenButton _addNewFolder = new();
+    private RadzenButton _deleteFolder = new();
+    private bool _isFolderSelected = false;
+    private bool _deleteFolderWithContents;
+    private Popup _createNewFolderDialog = new();
     private string? _errorMessage = string.Empty;
+    private Popup _deleteFolderConfirmationDialog = new();
+    private CurrentFolderViewModel? _currentFolder = new();
 
-    private readonly IList<Tuple<FileSystemItemViewModel,
-        RadzenDataGridColumn<FileSystemItemViewModel>>> selectedCellData =
+    private readonly IList<
+        Tuple<FileSystemItemViewModel,
+            RadzenDataGridColumn<FileSystemItemViewModel>>> _selectedCellData =
             new List<Tuple<FileSystemItemViewModel,
                 RadzenDataGridColumn<FileSystemItemViewModel>>>();
 
-    private RadzenButton? button;
-    private RadzenButton? button2;
-    private Popup? popup;
-    private Popup? popup2;
-    private bool _deleteFolderWithContents;
+    private FileManagerClient? _fileManagerClient;
 
     protected override void OnInitialized()
     {
-        AddAllDrivesToDisplayModel();
+        _fileManagerClient = new(_fileManager);
+        InitializeRootDirectory();
+        RefreshDirectoryContents();
     }
 
-    private void CreateNewFolder(string newDirectoryName)
+    private void InitializeRootDirectory()
     {
-        string newFolder = System.IO.Path
-            .Combine(_currentFolder!.FullPath, newDirectoryName);
         try
         {
-            _fileManager.CreateFolder(newFolder);
+            _fileManagerClient!.AddAllDrivesToDisplayModel();
         }
-        catch (PathArgumentException ex)
+        catch (AccessDeniedException ex)
         {
-            Console.WriteLine(ex.Message);
             _errorMessage = ex.Message;
         }
         catch (InnerErrorException ex)
         {
-            Console.WriteLine(ex.Message);
             _errorMessage = ex.Message;
         }
-        catch (AccessDeniedException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-        }
-
-        NavigateToSubFolder(_currentFolder.FullPath);
     }
 
-    private void DeleteFolder()
+    private void RefreshDirectoryContents()
+    {
+        _currentFolder = _fileManagerClient!.CurrentFolder;
+    }
+
+    private async Task OnCreateNewDirectory()
+    {
+        await JSRuntime.InvokeVoidAsync(
+            "eval",
+            "setTimeout(function(){ document" +
+            ".getElementById('search').focus(); }, 200)");
+    }
+
+    private async Task OnDeleteDirectory()
+    {
+        await JSRuntime.InvokeVoidAsync(
+            "eval",
+            "setTimeout(function(){ document" +
+            ".getElementById('search').focus(); }, 200)");
+    }
+
+    private async Task CloseCreateDirectoryDialog()
+    {
+        await _createNewFolderDialog.CloseAsync();
+    }
+
+    private async Task CloseDeleteDirectoryDialog()
+    {
+        await _deleteFolderConfirmationDialog.CloseAsync();
+    }
+
+    private async Task RequestCreateNewDirectory()
+    {
+        _errorMessage = null;
+        if (NewDirectoryName is not null)
+        {
+            await CreateNewDirectory();
+            return;
+        }
+
+        _errorMessage = "Directory name can not be empty.";
+    }
+
+    private async Task CreateNewDirectory()
+    {
+        CreateNewFolder();
+        RefreshDirectoryContents();
+        NewDirectoryName = null;
+        await CloseCreateDirectoryDialog();
+    }
+
+    private void CreateNewFolder()
     {
         try
         {
-            _fileManager.DeleteFolder(
-                selectedCellData[0].Item1.FullName,
-                _deleteFolderWithContents);
+            _fileManagerClient!.CreateNewFolder(NewDirectoryName!);
         }
         catch (PathArgumentException ex)
         {
-            Console.WriteLine(ex.Message);
             _errorMessage = ex.Message;
         }
         catch (InnerErrorException ex)
         {
-            Console.WriteLine(ex.Message);
             _errorMessage = ex.Message;
         }
         catch (AccessDeniedException ex)
         {
-            Console.WriteLine(ex.Message);
             _errorMessage = ex.Message;
+        }
+    }
+
+    private async Task ConfirmDeleteDirectory()
+    {
+        DeleteDirectory();
+        await CloseDeleteDirectoryDialog();
+        NavigateToDirectory(_currentFolder!.FullPath);
+        RefreshDirectoryContents();
+    }
+
+    private void NavigateToDirectory(string path)
+    {
+        try
+        {
+            _fileManagerClient!.NavigateToSubFolder(path);
+        }
+        catch (PathArgumentException ex)
+        {
+            _errorMessage = ex.Message;
+        }
+        catch (InnerErrorException ex)
+        {
+            _errorMessage = ex.Message;
+        }
+        catch (AccessDeniedException ex)
+        {
+            _errorMessage = ex.Message;
+        }
+    }
+
+    private void DeleteDirectory()
+    {
+        try
+        {
+            _fileManagerClient!.DeleteFolder(
+                        _selectedCellData[0].Item1.FullName,
+                        _deleteFolderWithContents);
+        }
+        catch (PathArgumentException ex)
+        {
+            _errorMessage = ex.Message;
+        }
+        catch (InnerErrorException ex)
+        {
+            _errorMessage = ex.Message;
+        }
+        catch (AccessDeniedException ex)
+        {
+            _errorMessage = ex.Message;
+        }
+    }
+
+    private void OnCellRender(
+        DataGridCellRenderEventArgs<FileSystemItemViewModel> args)
+    {
+        if (_selectedCellData
+            .Any(i => i.Item1 == args.Data && i.Item2 == args.Column))
+        {
+            args.Attributes.Add("style",
+                $"background-color: var(--rz-secondary-lighter);");
         }
     }
 
     private void Select(DataGridCellMouseEventArgs<FileSystemItemViewModel> args)
     {
-        var cellData = selectedCellData.FirstOrDefault(
+        var cellData = _selectedCellData.FirstOrDefault(
             i => i.Item1 == args.Data && i.Item2 == args.Column);
         if (cellData != null)
         {
-            selectedCellData.RemoveAt(0);
-            isFolderSelected = false;
+            _selectedCellData.RemoveAt(0);
+            _isFolderSelected = false;
         }
         else
         {
-            selectedCellData.Add(
+            _selectedCellData.Add(
                 new Tuple<FileSystemItemViewModel,
                 RadzenDataGridColumn<FileSystemItemViewModel>>(
                     args.Data, args.Column));
-            if (selectedCellData.Count > 1)
+            if (_selectedCellData.Count > 1)
             {
-                selectedCellData.RemoveAt(0);
+                _selectedCellData.RemoveAt(0);
             }
 
-            isFolderSelected = true;
+            _isFolderSelected = true;
         }
     }
 
     private void OnCellClick(
         DataGridCellMouseEventArgs<FileSystemItemViewModel> args)
     {
-        if (type == "Click")
-        {
-            Select(args);
-            _errorMessage = null;
-        }
+        Select(args);
+        _errorMessage = null;
     }
 
     private void OnCellDoubleClick(
         DataGridCellMouseEventArgs<FileSystemItemViewModel> args)
     {
-        if (type != "Click")
-        {
-            Select(args);
-        }
-
-        NavigateToSubFolder(args.Data.FullName);
-    }
-
-    private void NavigateToSubFolder(string path)
-    {
-        if (string.IsNullOrWhiteSpace(path))
-        {
-            _currentFolder = new();
-            AddAllDrivesToDisplayModel();
-            return;
-        }
-
-        DirectoryInformation dirInfo;
-        try
-        {
-            dirInfo = _fileManager.GetDirectoryInformation(path);
-        }
-        catch (PathArgumentException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-        catch (InnerErrorException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-
-        var temp = dirInfo.Parent;
-        if (_currentFolder!.InnerItems[0].FullName == path)
-        {
-            temp ??= string.Empty;
-        }
-        else
-        {
-            temp ??= dirInfo.Root;
-        }
-
-        IEnumerable<DirectoryInformation> directories;
-
-        try
-        {
-            directories = _fileManager.GetAllInnerDerictoriesInformation(path);
-        }
-        catch (PathArgumentException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-        catch (InnerErrorException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-        catch (AccessDeniedException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-
-        IEnumerable<FileInformation> files;
-        try
-        {
-            files = _fileManager.GetAllInnerFilesInfo(path);
-        }
-        catch (PathArgumentException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-        catch (InnerErrorException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-        catch (AccessDeniedException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-
-        _currentFolder = new();
-        _currentFolder = new CurrentFolderViewModel()
-        {
-            Name = path,
-            FullPath = path,
-            Parent = temp,
-            IsRoot = false,
-        };
-        _currentFolder.InnerItems.Add(CreateParentItem(temp));
-        AddAllFoldersToDisplayModel(directories);
-        AddAllFilesToDisplayModel(files);
-    }
-
-    private void AddAllFoldersToDisplayModel(IEnumerable<DirectoryInformation> folders)
-    {
-        List<FileSystemItemViewModel> temp = new();
-        foreach (var folder in folders)
-        {
-            temp.Add(new FileSystemItemViewModel()
-            {
-                Name = folder.Name,
-                FullName = folder.FullName,
-                Parent = folder.Parent,
-                Extension = string.Empty,
-                ModifiedDate = folder.ModifiedDate
-            });
-        }
-
-        _currentFolder!.InnerItems.AddRange(temp);
-    }
-
-    private void AddAllFilesToDisplayModel(IEnumerable<FileInformation> files)
-    {
-        List<FileSystemItemViewModel> temp = new();
-        foreach (var file in files)
-        {
-            temp.Add(new FileSystemItemViewModel()
-            {
-                Name = file.Name,
-                FullName = file.FullName,
-                Parent = string.Empty,
-                Extension = file.Extension,
-                Size = file.Size,
-                ModifiedDate = file.ModifiedDate
-            });
-        }
-
-        _currentFolder!.InnerItems.AddRange(temp);
-    }
-
-    private void AddAllDrivesToDisplayModel()
-    {
-        IEnumerable<DriveInformation> drives;
-        try
-        {
-            drives = _fileManager.GetAllDrives();
-        }
-        catch (AccessDeniedException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-        catch (InnerErrorException ex)
-        {
-            Console.WriteLine(ex.Message);
-            _errorMessage = ex.Message;
-            return;
-        }
-
-        List<FileSystemItemViewModel> temp = new();
-        foreach (var drive in drives)
-        {
-            temp.Add(new FileSystemItemViewModel()
-            {
-                Name = drive.Name,
-                FullName = drive.Name,
-                Parent = string.Empty,
-                Extension = string.Empty,
-                Size = drive.Size,
-            });
-        }
-
-        _currentFolder = new CurrentFolderViewModel()
-        {
-            IsRoot = true,
-        };
-        _currentFolder.InnerItems.AddRange(temp);
-    }
-
-    private FileSystemItemViewModel CreateParentItem(string parent)
-    {
-        return new FileSystemItemViewModel()
-        {
-            Name = "..",
-            FullName = parent,
-            Parent = parent,
-            Extension = string.Empty,
-            Size = 0,
-        };
-    }
-
-    private async Task OnOpen()
-    {
-        await JSRuntime.InvokeVoidAsync(
-            "eval",
-            "setTimeout(function(){ document.getElementById('search').focus(); }, 200)");
-    }
-
-    private async Task OnOpen2()
-    {
-        await JSRuntime.InvokeVoidAsync(
-            "eval",
-            "setTimeout(function(){ document.getElementById('search').focus(); }, 200)");
-    }
-
-    private async Task CloseCreateDirectoryDialog()
-    {
-        await popup!.CloseAsync();
-    }
-
-    private async Task CloseDeleteDirectoryDialog()
-    {
-        await popup2!.CloseAsync();
-    }
-
-    private async Task CreateNewDirectory()
-    {
-        _errorMessage = null;
-        if (NewDirectoryName == null)
-        {
-            _errorMessage = "Directory name can not be empty.";
-            return;
-        }
-
-        CreateNewFolder(NewDirectoryName!);
-        NewDirectoryName = null;
-        await CloseCreateDirectoryDialog();
-    }
-
-    private async Task ConfirmDeleteDirectory()
-    {
-        DeleteFolder();
-        await CloseDeleteDirectoryDialog();
-        NavigateToSubFolder(_currentFolder!.FullPath);
-    }
-
-    private void OnCellRender(DataGridCellRenderEventArgs<FileSystemItemViewModel> args)
-    {
-        if (selectedCellData
-            .Any(i => i.Item1 == args.Data && i.Item2 == args.Column))
-        {
-            args.Attributes.Add("style",
-                $"background-color: var(--rz-secondary-lighter);");
-        }
+        NavigateToDirectory(args.Data.FullName);
+        RefreshDirectoryContents();
     }
 }
---
+++ BlazorFileManager/Services/FileManagerClient.cs
@@ -0,0 +1,318 @@
+﻿using BlazorFileManager.Models;
+using FileManagerDomain.Exceptions;
+using FileManagerDomain.Interfaces;
+using FileManagerDomain.Models;
+
+namespace BlazorFileManager.Services;
+
+public class FileManagerClient
+{
+    private readonly IFileManager _fileManager;
+    public string ParentFolderDisplayName { get; set; } = "..";
+    public CurrentFolderViewModel CurrentFolder { get; set; } = new();
+
+    public FileManagerClient(IFileManager fileManager)
+    {
+        _fileManager = fileManager;
+    }
+
+    public void CreateNewFolder(string newDirectoryName)
+    {
+        string newFolder = Path
+            .Combine(CurrentFolder!.FullPath, newDirectoryName);
+        CreateFolder(newFolder);
+        NavigateToSubFolder(CurrentFolder.FullPath);
+    }
+
+    private void CreateFolder(string newFolder)
+    {
+        try
+        {
+            _fileManager.CreateFolder(newFolder);
+        }
+        catch (PathArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (InnerErrorException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (AccessDeniedException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+
+    public void DeleteFolder(string path, bool recursiveDeletion)
+    {
+        try
+        {
+            _fileManager.DeleteFolder(path,
+                recursiveDeletion);
+        }
+        catch (PathArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (InnerErrorException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (AccessDeniedException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+
+    public void NavigateToSubFolder(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            CurrentFolder = new();
+            AddAllDrivesToDisplayModel();
+            return;
+        }
+
+        InitializeDirectory(path);
+    }
+
+    private void InitializeDirectory(string path)
+    {
+        DirectoryInformation directoryInformation =
+            GetDirectoryInformation(path);
+        string parentDirectory = GetParentDirectory(path, directoryInformation);
+        IEnumerable<DirectoryInformation> directories = GetDirectories(path);
+        IEnumerable<FileInformation> files = GetFiles(path);
+        InitializeCurrentFolder(path, parentDirectory, directories, files);
+    }
+
+    private void InitializeCurrentFolder(string path, string temp,
+        IEnumerable<DirectoryInformation> directories,
+        IEnumerable<FileInformation> files)
+    {
+        CurrentFolder = new CurrentFolderViewModel()
+        {
+            Name = path,
+            FullPath = path,
+            Parent = temp,
+            IsRoot = false,
+        };
+        CurrentFolder.InnerItems.Add(CreateParentItem(temp));
+        AddAllFoldersToDisplayModel(directories);
+        AddAllFilesToDisplayModel(files);
+    }
+
+    private string GetParentDirectory(string path, DirectoryInformation directoryInformation)
+    {
+        string temp = directoryInformation.Parent;
+        if (CurrentFolder!.InnerItems[0].FullName == path)
+        {
+            temp ??= string.Empty;
+        }
+        else
+        {
+            temp ??= directoryInformation.Root;
+        }
+
+        return temp;
+    }
+
+    private IEnumerable<FileInformation> GetFiles(string path)
+    {
+        IEnumerable<FileInformation> files;
+        try
+        {
+            files = _fileManager.GetAllInnerFilesInfo(path);
+        }
+        catch (PathArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (InnerErrorException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (AccessDeniedException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+
+        return files;
+    }
+
+    private IEnumerable<DirectoryInformation> GetDirectories(string path)
+    {
+        IEnumerable<DirectoryInformation> directories;
+        try
+        {
+            directories = _fileManager
+                .GetAllInnerDerictoriesInformation(path);
+        }
+        catch (PathArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (InnerErrorException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (AccessDeniedException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+
+        return directories;
+    }
+
+    private DirectoryInformation GetDirectoryInformation(string path)
+    {
+        DirectoryInformation directoryInformation;
+        try
+        {
+            directoryInformation = _fileManager.GetDirectoryInformation(path);
+        }
+        catch (PathArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (InnerErrorException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+
+        return directoryInformation;
+    }
+
+    public void AddAllDrivesToDisplayModel()
+    {
+        IEnumerable<DriveInformation> drives = GetDrives();
+        List<FileSystemItemViewModel> drivesViewModels = GetDrivesViewModels(drives);
+
+        CurrentFolder = new CurrentFolderViewModel()
+        {
+            IsRoot = true,
+        };
+        CurrentFolder.InnerItems.AddRange(drivesViewModels);
+    }
+
+    private static List<FileSystemItemViewModel> GetDrivesViewModels(IEnumerable<DriveInformation> drives)
+    {
+        List<FileSystemItemViewModel> drivesViewModels = new();
+        foreach (var drive in drives)
+        {
+            drivesViewModels.Add(new FileSystemItemViewModel()
+            {
+                Name = drive.Name,
+                FullName = drive.Name,
+                Parent = string.Empty,
+                Extension = string.Empty,
+                Size = drive.Size,
+            });
+        }
+
+        return drivesViewModels;
+    }
+
+    private IEnumerable<DriveInformation> GetDrives()
+    {
+        IEnumerable<DriveInformation> drives;
+        try
+        {
+            drives = _fileManager.GetAllDrives();
+        }
+        catch (AccessDeniedException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (InnerErrorException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+
+        return drives;
+    }
+
+    private void AddAllFoldersToDisplayModel(
+        IEnumerable<DirectoryInformation> folders)
+    {
+        List<FileSystemItemViewModel> foldersViewModels =
+            GetFoldersViewModels(folders);
+
+        CurrentFolder!.InnerItems.AddRange(foldersViewModels);
+    }
+
+    private static List<FileSystemItemViewModel> GetFoldersViewModels(
+        IEnumerable<DirectoryInformation> folders)
+    {
+        List<FileSystemItemViewModel> foldersViewModels = new();
+        foreach (var folder in folders)
+        {
+            foldersViewModels.Add(new FileSystemItemViewModel()
+            {
+                Name = folder.Name,
+                FullName = folder.FullName,
+                Parent = folder.Parent,
+                Extension = string.Empty,
+                ModifiedDate = folder.ModifiedDate
+            });
+        }
+
+        return foldersViewModels;
+    }
+
+    private void AddAllFilesToDisplayModel(
+        IEnumerable<FileInformation> files)
+    {
+        List<FileSystemItemViewModel> filesViewModels = GetFilesViewModels(files);
+
+        CurrentFolder!.InnerItems.AddRange(filesViewModels);
+    }
+
+    private static List<FileSystemItemViewModel> GetFilesViewModels(IEnumerable<FileInformation> files)
+    {
+        List<FileSystemItemViewModel> filesViewModels = new();
+        foreach (var file in files)
+        {
+            filesViewModels.Add(new FileSystemItemViewModel()
+            {
+                Name = file.Name,
+                FullName = file.FullName,
+                Parent = string.Empty,
+                Extension = file.Extension,
+                Size = file.Size,
+                ModifiedDate = file.ModifiedDate
+            });
+        }
+
+        return filesViewModels;
+    }
+
+    private FileSystemItemViewModel CreateParentItem(string parent)
+    {
+        return new FileSystemItemViewModel()
+        {
+            Name = ParentFolderDisplayName,
+            FullName = parent,
+            Parent = parent,
+            Extension = string.Empty,
+            Size = 0,
+        };
+    }
+}
